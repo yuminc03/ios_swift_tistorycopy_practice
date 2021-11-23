a69386bb7135e0f03c45a9af5e2827208ad7ce30
Yumini <60338309+yuminc03@users.noreply.github.com>
2021-11-23 23:40:53 +0900

    2021-11-23: 알림 설정 UI 수정중...

--- TStoryApp/TStoryApp/Notification/NoticeViewController.swift
+++ TStoryApp/TStoryApp/Notification/NoticeViewController.swift
@@ -12,6 +12,7 @@ class NoticeViewController: UIViewController {
     var noticeModel = NoticeModel()
     var selectedCategoryIndex: Int = 0
     var myBlogOfProfileModel = MyBlogOfProfileModel()
+    var blogSearchModel = BlogSearchModel()
     
     lazy var navigationBarView: NavigationBarView = {
         let navigationView = NavigationBarView()
@@ -119,6 +120,89 @@ class NoticeViewController: UIViewController {
             ]
         )
         noticeModel = model
+        
+        
+        blogSearchModel = BlogSearchModel(
+        searchResultModal: [
+            SearchResultModel(
+                searchWords: "swift",
+                searchDate: "11. 22."
+            ),
+            SearchResultModel(
+                searchWords: "ㄹㄹㅅㅎㄹㄱ",
+                searchDate: "11. 17."
+            ),
+            SearchResultModel(
+                searchWords: "알바천국",
+                searchDate: "11. 16."
+            )
+        ])
+        
+        
+        myBlogOfProfileModel = MyBlogOfProfileModel(
+            profileName: "엘리아",
+            profileEmail: "yuminc03@gmail.com",
+            repBlogName: "천천히 해도 괜찮아",
+            appVersion: "2.4.7",
+            myBlogsModel: [
+                MyBlogsModel(
+                    blogName: "천천히 해도 괜찮아",
+                    blogUrl: "dpffldk.tistory.com"
+                )
+            ],
+            notificationModel:
+                NotificationModel(
+                    pushAlarm: true,
+                    commentAlarm: true,
+                    teamBlogInviteAlarm: true,
+                    subscibeAlarm: true,
+                    doNotdisturbMode: false
+                ),
+            announcementModel: [
+                AnnouncementModel(
+                    announceTitle: "[안내] 모바일웹이 개선되었습니다.",
+                    announceImageName: "image1.png",
+                    announceLikeNum: 94,
+                    announceCommentNum: 95,
+                    announceDate: "2021. 10. 12."
+                ),
+                AnnouncementModel(
+                    announceTitle: "[참고] 2차 도메인이 루트도메인인지 확인하는 방법.",
+                    announceImageName: "image2.png",
+                    announceLikeNum: 17,
+                    announceCommentNum: 8,
+                    announceDate: "2021. 9. 1."
+                ),
+                AnnouncementModel(
+                    announceTitle: "[안내] abs.txt 파일 문제가 해결됐습니다.",
+                    announceImageName: "image3.png",
+                    announceLikeNum: 81,
+                    announceCommentNum: 65,
+                    announceDate: "2021. 9. 1."
+                ),
+                AnnouncementModel(
+                    announceTitle: "[안내] 티스토리 개인정보처리방침 변경 안내",
+                    announceImageName: "image4.png",
+                    announceLikeNum: 23,
+                    announceCommentNum: 7,
+                    announceDate: "2021. 8. 2."
+                ),
+                AnnouncementModel(
+                    announceTitle: "[안내] 수익 기능이 다양해졌습니다.",
+                    announceImageName: "image5.png",
+                    announceLikeNum: 118,
+                    announceCommentNum: 76,
+                    announceDate: "2021. 7. 27"
+                ),
+                AnnouncementModel(
+                    announceTitle: "[수익 사용법] 텐핑 광고 설정하기",
+                    announceImageName: "image6.png",
+                    announceLikeNum: 17,
+                    announceCommentNum: 6,
+                    announceDate: "2021. 7. 14."
+                )
+            ]
+        )
     }
     
     private func setConstraints() {
@@ -141,11 +225,10 @@ class NoticeViewController: UIViewController {
     
     @objc private func blogProfileButtonDidTapped(button: UIButton) {
         tabBarController?.tabBar.isHidden = true
-        let vc = MyBlogOfProfileViewController()
+        let vc = MyBlogOfProfileViewController(myBlogOfProfileModel: self.myBlogOfProfileModel)
         vc.modalPresentationStyle = .overCurrentContext
         vc.delegate = self
         self.present(vc, animated: false)
-        self.myBlogOfProfileModel = vc.myBlogOfProfileModel
     }
     
 }
