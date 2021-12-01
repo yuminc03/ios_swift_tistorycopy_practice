c64afee4b8134662f4a9f4db00fcfb27eaef016f
Yumini <60338309+yuminc03@users.noreply.github.com>
2021-12-01 23:28:28 +0900

    2021-12-1: profile, home, MyBlogofProfile

--- TStoryApp/TStoryApp/MyBlogOfProfile/MyBlogOfProfileViewController.swift
+++ TStoryApp/TStoryApp/MyBlogOfProfile/MyBlogOfProfileViewController.swift
@@ -42,6 +42,92 @@ class MyBlogOfProfileViewController: UIViewController {
         super.init(nibName: nil, bundle: nil)
     }
     
+    func configulations() {
+        let searchModel = BlogSearchModel(
+        searchResultModal: [
+            SearchResultModel(
+                searchWords: "swift",
+                searchDate: "11. 22."
+            ),
+            SearchResultModel(
+                searchWords: "ㄹㄹㅅㅎㄹㄱ",
+                searchDate: "11. 17."
+            ),
+            SearchResultModel(
+                searchWords: "알바천국",
+                searchDate: "11. 16."
+            )
+        ])
+        
+        
+        let profileModel = MyBlogOfProfileModel(
+            profileName: "엘리아",
+            profileEmail: "yuminc03@gmail.com",
+            repBlogName: "천천히 해도 괜찮아",
+            appVersion: "2.4.8",
+            myBlogsModel: [
+                MyBlogsModel(
+                    blogName: "천천히 해도 괜찮아",
+                    blogUrl: "dpffldk.tistory.com"
+                )
+            ],
+            notificationModel:
+                NotificationModel(
+                    pushAlarm: true,
+                    commentAlarm: true,
+                    teamBlogInviteAlarm: true,
+                    subscibeAlarm: true,
+                    doNotdisturbMode: false
+                ),
+            announcementModel: [
+                AnnouncementModel(
+                    announceTitle: "[안내] 모바일웹이 개선되었습니다.",
+                    announceImageName: "image1.png",
+                    announceLikeNum: 94,
+                    announceCommentNum: 95,
+                    announceDate: "2021. 10. 12."
+                ),
+                AnnouncementModel(
+                    announceTitle: "[참고] 2차 도메인이 루트도메인인지 확인하는 방법.",
+                    announceImageName: "image2.png",
+                    announceLikeNum: 17,
+                    announceCommentNum: 8,
+                    announceDate: "2021. 9. 1."
+                ),
+                AnnouncementModel(
+                    announceTitle: "[안내] abs.txt 파일 문제가 해결됐습니다.",
+                    announceImageName: "image3.png",
+                    announceLikeNum: 81,
+                    announceCommentNum: 65,
+                    announceDate: "2021. 9. 1."
+                ),
+                AnnouncementModel(
+                    announceTitle: "[안내] 티스토리 개인정보처리방침 변경 안내",
+                    announceImageName: "image4.png",
+                    announceLikeNum: 23,
+                    announceCommentNum: 7,
+                    announceDate: "2021. 8. 2."
+                ),
+                AnnouncementModel(
+                    announceTitle: "[안내] 수익 기능이 다양해졌습니다.",
+                    announceImageName: "image5.png",
+                    announceLikeNum: 118,
+                    announceCommentNum: 76,
+                    announceDate: "2021. 7. 27"
+                ),
+                AnnouncementModel(
+                    announceTitle: "[수익 사용법] 텐핑 광고 설정하기",
+                    announceImageName: "image6.png",
+                    announceLikeNum: 17,
+                    announceCommentNum: 6,
+                    announceDate: "2021. 7. 14."
+                )
+            ]
+        )
+        
+        myBlogOfProfileModel = profileModel
+    }
+    
     private func setConstriants() {
         NSLayoutConstraint.activate([
             myBlogOfProfileView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
