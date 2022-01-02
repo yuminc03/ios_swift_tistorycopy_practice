4de3b83c213c8b5f2abc6e5a96be6754ec720cd4
Yumini <60338309+yuminc03@users.noreply.github.com>
2022-01-02 21:43:11 +0900

    2021.1.2.

--- TStoryApp/TStoryApp/Writing/View/PublishingView/View.swift
+++
@@ -1,8 +0,0 @@
-//
-//  View.swift
-//  TStoryApp
-//
-//  Created by LS-NOTE-00106 on 2021/11/02.
-//
-
-import Foundation
--- TStoryApp/TStoryApp/Writing/View/PublishingView/View/PostContentsTagSettingView/File.swift
+++
@@ -1,8 +0,0 @@
-//
-//  File.swift
-//  TStoryApp
-//
-//  Created by LS-NOTE-00106 on 2021/11/03.
-//
-
-import Foundation
